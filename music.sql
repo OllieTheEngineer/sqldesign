ccf21dd4ac4925dca477ccd40ca4bc3a508e2f93
ollie <olivinanzali07@gmail.com>
2022-06-07 23:37:34 -0400

    added music

--- music.sql
+++ music.sql
@@ -12,22 +12,42 @@ CREATE TABLE songs
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
   duration_in_seconds INTEGER NOT NULL,
+  artists TEXT[] NOT NULL
+);
+
+CREATE TABLE album
+(
+  id SERIAL PRIMARY KEY,
+  songs_id INTEGER REFERENCES songs,
+  producers TEXT[] NOT NULL,
   release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+  album_name TEXT [] NOT NULL
 );
 
 INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
+  (title, duration_in_seconds, artists)
 VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+  ('MMMBop', 238,'{"Hanson"}'),
+  ('Bohemian Rhapsody', 355, '{"Queen"}'),
+  ('One Sweet Day', 282, '{"Mariah Cary", "Boyz II Men"}'),
+  ('Shallow', 216, '{"Lady Gaga", "Bradley Cooper"}'),
+  ('How You Remind Me', 223, '{"Nickelback"}'),
+  ('New York State of Mind', 276, '{"Jay Z", "Alicia Keys"}'),
+  ('Dark Horse', 215, '{"Katy Perry", "Juicy J"}'),
+  ('Moves Like Jagger', 201, '{"Maroon 5", "Christina Aguilera"}'),
+  ('Complicated', 244, '{"Avril Lavigne"}'),
+  ('Say My Name', 240, '{"Destiny''s Child"}');
+
+INSERT INTO album
+(producers, album_name, release_date)
+VALUES
+('{"Dust Brothers", "Stephen Lironi"}', '{"Middle of Nowhere"}', '04-15-1997'),
+('{"Roy Thomas Baker"}', '{"A Night at the Opera"}', '10-31-1975'),
+('{"Walter Afanasieff"}', '{"Daydream"}', '11-14-1995'),
+('{"Benjamin Rice"}', '{"A Star Is Born"}', '09-27-2018'),
+('{"Rick Parashar"}', '{"Silver Side Up"}', '08-21-2001'),
+('{"Al Shux"}', '{"The Blueprint 3"}', '10-20-2009'),
+('{"Max Martin", "Cirkut"}', '{"Prism"}', '12-17-2013'),
+('{"Shellback", "Benny Blanco"}', '{"Hands All Over"}', '06-21-2011'),
+('{"The Matrix"}', '{"Let Go"}', '05-14-2002'),
+('{"Darkchild"}', '{"The Writings on the Wall"}', '11-07-1999');
